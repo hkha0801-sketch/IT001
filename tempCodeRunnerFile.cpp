0f493d09877a9365e6e2b039f2f76ae6a692c0e7
HK07 <hkha0801@gmail.com>
2025-12-09 00:42:58 +0700

    v1

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,25 @@
-std::cin >> x;
+void play(int* arr, int n){
+    int buoc=0;
+    for (int i = 0; i < n; i++){
+        if (arr[i+1]==1 && arr[i+2]==1){
+            cout << -1;
+            return ;
+        }
+        else {
+            if (arr[i+1]==1&& (i+=2)<n){
+                i+=1;
+                buoc+=1;
+            }
+               
+            else if ((i+=1)<n){
+                buoc+=1;
+            }
+            else if (arr[i+1]==0 && arr[i+2]==0 && (i+=2)<n){
+                i+=2;
+                buoc+=1;
+            }
+
+        }
+    }
+    cout << buoc;
+}
